f03f2e7395f12dca71d8c11839134da71f383feb
Timothée Poisot <tim@poisotlab.io>
2016-05-13 20:28:50 -0400

    Added some very simple code + tests
    For the moment, all the tests fails by design. I need to get a better idea of
    how things will work out before writing them. But at least, the package reads a
    list of references are creates an object.

--- shelf.js
+++ shelf.js
@@ -1,1 +1,34 @@
 var diacritics = require('diacritics');
+var fs = require('fs');
+var os = require('os');
+
+// console.log(os.homedir());
+
+function Library(path) {
+  if (path === undefined) {
+    console.log("No path given, assuming defaut location");
+    var home = process.env.HOME || process.env.USERPROFILE;
+    path = home + "/.pandoc";
+  }
+  this.path = path;
+  console.log("Using path: " + this.path);
+
+  // Build the path for records, files, and file db
+  this.records = this.path + "/records/"
+
+  this.entries = {};
+  var files = fs.readdirSync(this.records);
+  for (var i = 0; i < files.length; i++) {
+    var entry = require(this.records + files[i]);
+    this.entries[entry.id] = entry;
+  }
+
+}
+
+Library.prototype.entry = function(id) {
+  console.log(this.entries[id]);
+}
+
+var lib = new Library();
+lib.entry("gravel_tti");
+lib.entry("poisot_dsi");
--- test/test.js
+++ test/test.js
@@ -2,7 +2,27 @@ var assert = require('chai').assert;
 
 // TODO add test data
 
+describe('Library', function() {
+
+  describe('loading', function() {
+    it('should create a default path if none is given', function () {
+      assert.equal(0, 1);
+    });
+  });
+
+  describe('getting entries', function() {
+    it('should return something when a valid key is accessed', function () {
+      assert.equal(0, 1);
+    });
+    it('should return undefined when an invalid key is accessed', function () {
+      assert.equal(0, 1);
+    });
+  });
+
+});
+
 describe('Unique ID', function() {
+
   describe('names', function () {
     it('should return anonymous if there are no authors', function () {
       assert.equal(0, 1);
@@ -17,9 +37,11 @@ describe('Unique ID', function() {
       assert.equal(0, 1);
     });
   });
+
   describe('unicity', function () {
     it('should return a unique number at the end if there is a duplicate', function () {
       assert.equal(0, 1);
     });
   });
+
 });
