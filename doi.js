5f8fb09999e16bc699f203223698f9a17fcea1fd
Timothée Poisot <tim@poisotlab.io>
2016-05-20 17:37:42 -0400

    == -> ===

--- doi.js
+++ doi.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 function from_doi(doi) {
   var url = "http://api.crossref.org/works/" + doi + "/transform/application/vnd.citationstyles.csl+json";
   var res = request('GET', url);
-  if(res.statusCode == 200) {
+  if(res.statusCode === 200) {
     return JSON.parse(res.getBody('utf-8'));
   } else {
     return undefined;
